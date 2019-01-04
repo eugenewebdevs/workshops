bfcd97d1d2c5d5c2942ffea568ea91f73bcf26a5
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-01-04 14:25:41 -0800

    use async/await

--- workshops-2019/microservice/lambdas/node/lambda_function.js
+++ workshops-2019/microservice/lambdas/node/lambda_function.js
@@ -1,17 +1,14 @@
-exports.handler = function(event, context, callback) {
+exports.handler = async function(event, context) {
     const url = 'https://s3-us-west-2.amazonaws.com/eugenewebdevs/workshops-2019/sample.json';
 
-    fetch(url)
-        .then(response => {
-            const data = response.json;
-            console.log({ data });
-            callback(null, data);
-            return true;
-        })
-        .catch(error => {
-            console.log({ error });
-            callback('An error occurred.');
-            return false;
-        });
+    try {
+        const response = await fetch(url);
+        const data = response.json();
+        console.log({ data });
+        return data;
+    } catch (error) {
+        console.error({ error });
+        throw new Error(`Uh oh. An error occurred while fetching '${url}'.`)
+    }
  }
  
