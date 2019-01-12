a06f6385dfe4c8dac1a6dc705d5e21e74ccdfb48
antonioortegajr <antonioortegajr@gmail.com>
2019-01-12 10:50:44 -0800

    change node to use standard http library

--- workshops-2019/microservice/lambdas/go/README.md
+++ workshops-2019/microservice/lambdas/go/README.md
@@ -8,6 +8,10 @@ This lambda is written using the AWS native Go support. Go version 1.x
 
 You will write your go code locally. For simple function like the http request in this repo, you can simply create your function with the AWS handler function in mind.
 
+You will need to install the Lambda dependency. 
+
+`go get github.com/aws/aws-lambda-go/lambda`
+
 You will need to then compile and zip your code. You can then upload to the lambda.
 
 Build your code with 
--- workshops-2019/microservice/lambdas/node/lambda_function.js
+++ workshops-2019/microservice/lambdas/node/lambda_function.js
@@ -1,14 +1,27 @@
-exports.handler = async function(event, context) {
-    const url = 'https://api.themoviedb.org/3/discover/movie?api_key=372f168ae29875f30eb9f0cda74358c1';
+const http = require('http');
 
-    try {
-        const response = await fetch(url);
-        const data = response.json();
-        console.log({ data });
-        return data;
-    } catch (error) {
-        console.error({ error });
-        throw new Error(`Uh oh. An error occurred while fetching '${url}'.`)
-    }
- }
- 
+exports.handler = async (event, context) => {
+    
+    return new Promise((resolve, reject) => {
+        const options = {
+            host: 'api.themoviedb.org',
+            path: '/3/discover/movie?api_key=372f168ae29875f30eb9f0cda74358c1',
+            method: 'GET'
+        };
+
+        const req = http.request(options, (res) => {
+          res.setEncoding('utf8');    
+          res.on('data', function (chunk) {
+             resolve('BODY: ' + chunk);
+            });
+        });
+
+        req.on('error', (e) => {
+          reject(e.message);
+        });
+
+        // send the request
+        req.write('data');
+        req.end();
+    });
+};
