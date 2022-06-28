d5bb793fe4f5a97ce569e037f62dca0e27c02cff
Ryan Olds <ryanrolds@gmail.com>
2022-06-27 21:26:30 -0700

    Working example

--- workshops-2022/docker/exercises/6-database/app/index.js
+++ workshops-2022/docker/exercises/6-database/app/index.js
@@ -1,7 +1,7 @@
 import {createConnection} from 'mysql';
 
 const connection = createConnection({
-    host: process.env.MYDSQL_HOST,
+    host: process.env.MYSQL_HOST,
     database: process.env.MYSQL_DATABASE,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
